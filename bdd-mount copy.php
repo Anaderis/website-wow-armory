0902508bce216cea2c4fa4a565e35d5a2a8e86c2
Anaïs Kajjaj <100074659+Anaderis@users.noreply.github.com>
2024-03-12 12:14:48 +0100

    dd

--- bdd-mount copy.php
+++ bdd-mount copy.php
@@ -1,5 +1,33 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+
+
+
+//On essaie de se connecter
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=wow armory", $username, $password);
+    //On définit le mode d'erreur de PDO sur Exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $dbco = $conn;
+}
+
+/*On capture les exceptions si une exception est lancée et on affiche
+ *les informations relatives à celle-ci*/ catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+
+/* On ferme la connexion */
+$conn = null;
+
+?>
+
 <!DOCTYPE html>
 <html>
+
+
 
 <head>
 
@@ -7,17 +35,30 @@
     <meta charset="utf-8" />
     <title>WoW Collection</title>
 
-    <link rel="stylesheet" href="./css/style-equipment.css" />
+
 
     <!-- Footer -->
     <!-- <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css" />
-
+   
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Titillium+Web&display=swap" rel="stylesheet" /> -->
+
+    <link rel="stylesheet" href="./css/style-equipment.css" />
+
+    <style>
+        form {
+            background-image: url('Assets/header-mounts.jpg');
+            background-size: cover;
+            height: 600px;
+            width: 100%;
+        }
+    </style>
+
+
 </head>
 
 <body>
@@ -54,14 +95,7 @@
 
         <form action="bdd-mount.php" method="post" class="formMount">
 
-            <style>
-                form {
-                    background-image: url('Assets/header-mounts.jpg');
-                    background-size: cover;
-                    height: 600px;
-                    width: 100%;
-                }
-            </style>
+
 
             <!-- /*------------------------ FILTRES MONTURES-------------------*/ -->
 
@@ -164,49 +198,32 @@
 
 
 
+
+
+
     <?php
-    $servername = 'localhost';
-    $username = 'root';
-    $password = '';
 
-
-
-    //On essaie de se connecter
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=wow armory", $username, $password);
-        //On définit le mode d'erreur de PDO sur Exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $dbco = $conn;
-
-
-        //On récupère les infos de la table 
-    
+    if (isset($_POST["submit"])) {
 
 
 
 
-        if (isset($_POST["submit"])) {
+        // Godefroy
+        // Step 1
+        // Pour chaque paramètre, on peut créé un boolean correspondant
+        $difficulty = $_POST["difficulty"];
+        $haveDifficulty = false;
+        $source = $_POST["source"];
+        $haveSource = false;
+        $extension = $_POST["extension"];
+        $haveExtension = false;
+        $faction = $_POST["faction"];
+        $haveFaction = false;
+        $type = $_POST["type"];
+        $haveType = false;
 
 
-
-
-            // Godefroy
-            // Step 1
-            // Pour chaque paramètre, on peut créé un boolean correspondant
-            $difficulty = $_POST["difficulty"];
-            $haveDifficulty = false;
-            $source = $_POST["source"];
-            $haveSource = false;
-            $extension = $_POST["extension"];
-            $haveExtension = false;
-            $faction = $_POST["faction"];
-            $haveFaction = false;
-            $type = $_POST["type"];
-            $haveType = false;
-
-
-            $sqlQuery = 'SELECT * FROM t_monture 
+        $sqlQuery = 'SELECT * FROM t_monture 
             INNER JOIN tj_m_appartient_fa_mafa on tj_m_appartient_fa_mafa.M_Id=t_monture.M_Id 
             INNER JOIN t_m_faction_mfa on t_m_faction_mfa.MFA_Id=tj_m_appartient_fa_mafa.MFA_Id 
             INNER JOIN t_m_difficulte_mdi on t_m_difficulte_mdi.MDI_Id=t_monture.MDI_Id 
@@ -215,114 +232,104 @@
             INNER JOIN t_m_type_mty on t_m_type_mty.MTY_Id=t_monture.MTY_Id';
 
 
-            // Godefroy
-            // Step 2
-            // On passe le boolean à True pour chaque critère utilisé.
-            if (!empty($_POST['difficulty'])) // si une region à été choisie
-            {
-                $where[] = ' MDI_Nom = ' . ':difficulty';
-                $haveDifficulty = true;
-            }
-            if (!empty($_POST['source'])) // si une region à été choisie
-            {
-                $where[] = ' MO_Nom = ' . ':source';
-                $haveSource = true;
-            }
-            if (!empty($_POST['extension'])) // si une region à été choisie
-            {
-                $where[] = ' ME_Nom = ' . ':extension';
-                $haveExtension = true;
-            }
-            if (!empty($_POST['faction'])) // si une region à été choisie
-            {
-                $where[] = ' MFA_Nom = ' . ':faction';
-                $haveFaction = true;
-            }
-            if (!empty($_POST['type'])) // si une region à été choisie
-            {
-                $where[] = ' MTY_Nom = ' . ':type';
-                $haveType = true;
-            }
+        // Godefroy
+        // Step 2
+        // On passe le boolean à True pour chaque critère utilisé.
+        if (!empty($_POST['difficulty'])) // si une region à été choisie
+        {
+            $where[] = ' MDI_Nom = ' . ':difficulty';
+            $haveDifficulty = true;
+        }
+        if (!empty($_POST['source'])) // si une region à été choisie
+        {
+            $where[] = ' MO_Nom = ' . ':source';
+            $haveSource = true;
+        }
+        if (!empty($_POST['extension'])) // si une region à été choisie
+        {
+            $where[] = ' ME_Nom = ' . ':extension';
+            $haveExtension = true;
+        }
+        if (!empty($_POST['faction'])) // si une region à été choisie
+        {
+            $where[] = ' MFA_Nom = ' . ':faction';
+            $haveFaction = true;
+        }
+        if (!empty($_POST['type'])) // si une region à été choisie
+        {
+            $where[] = ' MTY_Nom = ' . ':type';
+            $haveType = true;
+        }
 
 
 
 
-            if (isset($where)) {
-                $sqlQuery .= " WHERE " . implode(' AND ', $where);
-            }
+        if (isset($where)) {
+            $sqlQuery .= " WHERE " . implode(' AND ', $where);
+        }
 
 
 
-            // Godefroy
-            // Step 3
-            // Les IFs sont basiques, on peut les mettre chacun sur une ligne 
-            // pour que ça soit plus facile à comprendre/lire
-            $sth = $dbco->prepare($sqlQuery);
-            if ($haveFaction) {
-                $sth->bindParam(':faction', $faction, PDO::PARAM_STR);
-            }
-            if ($haveExtension) {
-                $sth->bindParam(':extension', $extension, PDO::PARAM_STR);
-            }
-            if ($haveSource) {
-                $sth->bindParam(':source', $source, PDO::PARAM_STR);
-            }
-            if ($haveDifficulty) {
-                $sth->bindParam(':difficulty', $difficulty, PDO::PARAM_STR);
-            }
-            if ($haveType) {
-                $sth->bindParam(':type', $type, PDO::PARAM_STR);
-            }
+        // Godefroy
+        // Step 3
+        // Les IFs sont basiques, on peut les mettre chacun sur une ligne 
+        // pour que ça soit plus facile à comprendre/lire
+        $sth = $dbco->prepare($sqlQuery);
+        if ($haveFaction) {
+            $sth->bindParam(':faction', $faction, PDO::PARAM_STR);
+        }
+        if ($haveExtension) {
+            $sth->bindParam(':extension', $extension, PDO::PARAM_STR);
+        }
+        if ($haveSource) {
+            $sth->bindParam(':source', $source, PDO::PARAM_STR);
+        }
+        if ($haveDifficulty) {
+            $sth->bindParam(':difficulty', $difficulty, PDO::PARAM_STR);
+        }
+        if ($haveType) {
+            $sth->bindParam(':type', $type, PDO::PARAM_STR);
+        }
 
-            $sth->execute();
+        $sth->execute();
 
-            //On affiche les infos de la table
-            $resultat = $sth->fetchAll(PDO::FETCH_ASSOC);
-            $keys = array_keys($resultat);
-            // for ($i = 0; $i < count($resultat); $i++) {
+        //On affiche les infos de la table
+        $resultat = $sth->fetchAll(PDO::FETCH_ASSOC);
+        $keys = array_keys($resultat);
+        // for ($i = 0; $i < count($resultat); $i++) {
     
 
-            foreach ($resultat as $resultats) {
-                ?>
-                <section>
-                    <div class="actu">
-                        <article class="article">
-                            <div class="articleMount">
-                                <div class="textMount">
-                                    <h3>
-                                        <?php echo $resultats['M_Nom'] ?>
-                                    </h3>
-                                    <p>
-                                        <?php echo $resultats['MO_Nom'] ?>
-                                        <?php echo $resultats['MDI_Nom'] ?>
-                                        <?php echo $resultats['ME_Nom'] ?>
-                                        <?php echo $resultats['MFA_Nom'] ?>
-                                        <?php echo $resultats['MTY_Nom'] ?>
+        foreach ($resultat as $resultats) {
+            ?>
+            <section>
+                <div class="actu">
+                    <article class="article">
+                        <div class="articleMount">
+                            <div class="textMount">
+                                <h3>
+                                    <?php echo $resultats['M_Nom'] ?>
+                                </h3>
+                                <p>
+                                    <?php echo $resultats['MO_Nom'] ?>
+                                    <?php echo $resultats['MDI_Nom'] ?>
+                                    <?php echo $resultats['ME_Nom'] ?>
+                                    <?php echo $resultats['MFA_Nom'] ?>
+                                    <?php echo $resultats['MTY_Nom'] ?>
 
-                                    </p>
-                                    <button class="read" type="button">Lire la suite</button>
-                                </div>
-
-                                <img src="<?php echo $resultats['chemin_image'] ?>" class="photoMount" />
+                                </p>
+                                <button class="read" type="button">Lire la suite</button>
                             </div>
-                    </div>
-                    </article>
-                    </div>
-                </section>
 
-                <?php
-            }
+                            <img src="<?php echo $resultats['chemin_image'] ?>" class="photoMount" />
+                        </div>
+                </div>
+                </article>
+                </div>
+            </section>
+
+            <?php
         }
     }
-
-    /*On capture les exceptions si une exception est lancée et on affiche
-     *les informations relatives à celle-ci*/ catch (PDOException $e) {
-        echo "Erreur : " . $e->getMessage();
-    }
-
-    /* On ferme la connexion */
-    $conn = null;
-
     ?>
 
 </body>
